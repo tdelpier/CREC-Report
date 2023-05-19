f51dd478012b04a8765f6c944394cb0e3f83ee36
tdelpier <tdelpier@gmail.com>
2023-05-19 07:38:22 -0400

    1st commit

---
+++ setup.R
@@ -0,0 +1,19 @@
+
+
+
+library(tidyverse)
+library(TannersTools)
+library(lubridate)
+library(scales)
+library(directlabels)
+library(fredr)
+library(ggtext)
+library(ggalt)
+
+
+
+
+mea_red <- "#C10230" 
+mea_blue <- "#003057"
+mea_green <- "#00A651"
+line_width <- 1
